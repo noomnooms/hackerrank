9c60ebb70132eff437aa02a64d7835b023a04078
naomialidinata <naomi.alidinata@gmail.com>
2023-10-30 17:29:17 +0800

    edit

--- Problem Solving/Algorithms/diagonal-difference.js
+++ Problem Solving/Algorithms/diagonal-difference.js
@@ -4,8 +4,6 @@ function diagonalDifference(arr) {
     var sum_a = 0, sum_b = 0;
     var i = 0, j = arr[0].length-1;
     
-    // var mid = false;
-    // Math.ceil(3/2)
     arr.forEach((element) => {
         sum_a += element[i];
         sum_b += element[j];
